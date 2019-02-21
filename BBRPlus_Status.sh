c595eb1cae064852755f5235bb787d431f56504c
AntonyWilsonClub <46566514+AntonyWilsonClub@users.noreply.github.com>
2019-02-21 15:23:00 +0800

    Create BBRPlus_Status.sh

---
+++ BBRPlus_Status.sh
@@ -0,0 +1,26 @@
+kernel_version=`uname -r | awk -F "-" '{print $1}'`
+	kernel_version_full=`uname -r`
+	if [[ ${kernel_version_full} = "4.14.91-bbrplus" ]]; then
+		kernel_status="BBRplus"
+	else 
+		kernel_status="noinstall"
+	fi
+	if [[ ${kernel_status} == "BBRplus" ]]; then
+		run_status=`grep "net.ipv4.tcp_congestion_control" /etc/sysctl.conf | awk -F "=" '{print $2}'`
+		if [[ ${run_status} == "bbrplus" ]]; then
+			run_status=`lsmod | grep "bbrplus" | awk '{print $1}'`
+			if [[ ${run_status} == "tcp_bbrplus" ]]; then
+				run_status="BBRplus启动成功"
+			else 
+				run_status="BBRplus启动失败"
+			fi
+		else 
+			run_status="未安装加速模块"
+		fi
+	fi
+  if [[ ${kernel_status} == "noinstall" ]]; then
+    echo -e " 当前状态: ${Green_font_prefix}未安装${Font_color_suffix} 加速内核 ${Red_font_prefix}请先安装内核${Font_color_suffix}"
+	else
+		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} ${_font_prefix}${kernel_status}${Font_color_suffix} 加速内核 , ${Green_font_prefix}${run_status}${Font_color_suffix}"
+		
+	fi
